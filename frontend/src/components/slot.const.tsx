cbb3325a9fda2d64c9bc83d8fad5f1a4ba0120f6
Mateusz Binięda <mat.binieda@gmail.com>
2025-02-02 23:52:03 +0100

    add slot machine start

---
+++ frontend/src/components/slot.const.tsx
@@ -0,0 +1,5 @@
+export const SYMBOLS = ["🍒", "🍋", "🍊", "🍇", "🔔", "⭐"]
+export const REEL_WIDTH = 160
+export const REEL_HEIGHT = 300
+export const NUMBER_OF_REELS = 3
+export const TIMEOUT_SPINNING = 2000
